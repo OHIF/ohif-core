ae64e2615d0190ffa128012364086673b060f310
Gustavo Lelis <galelis@gmail.com>
2019-02-15 17:00:27 -0200

    feat(measurementTable): Exporting LesionTracker and OHIF set of measurement tools

--- src/measurements/index.js
+++ src/measurements/index.js
@@ -4,12 +4,16 @@ import getDescription from './lib/getDescription';
 import getImageAttributes from './lib/getImageAttributes';
 import getImageIdForImagePath from './lib/getImageIdForImagePath';
 import syncMeasurementAndToolData from './lib/syncMeasurementAndToolData';
+import ltTools from './ltTools';
+import ohifTools from './ohifTools';
 import './configuration';
 
 const measurements = {
   TimepointApi,
   MeasurementApi,
   MeasurementHandlers,
+  ltTools,
+  ohifTools,
   getDescription,
   getImageAttributes,
   getImageIdForImagePath,
