426ca808f0781797c7aa9a46d70b03a31c514826
Gustavo André Lelis <galelis@gmail.com>
2019-03-01 09:42:10 -0300

    fix(hotkeys): Import jquery.hotkeys that was missing (#7)

--- src/hotkeys.js
+++ src/hotkeys.js
@@ -1,9 +1,7 @@
 import { HotkeysManager } from './classes/HotkeysManager';
 import jquery from 'jquery';
 window.$ = window.jQuery = jquery;
-
-// TODO: Need to remove this because it's breaking other usages..
-//import 'jquery.hotkeys';
+require('jquery.hotkeys');
 
 // Create hotkeys namespace using a HotkeysManager class instance
 const hotkeys = new HotkeysManager();
