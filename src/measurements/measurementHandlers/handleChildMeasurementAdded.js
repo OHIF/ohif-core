248ccd491e39551a7b4ac6045f246edfe1e8626e
Erik Ziegler <erik.sweed@gmail.com>
2019-03-06 23:06:01 +0100

    fix(MeasurementApi): Clean up retrieveMeasurements

--- src/measurements/measurementHandlers/handleChildMeasurementAdded.js
+++ src/measurements/measurementHandlers/handleChildMeasurementAdded.js
@@ -2,7 +2,6 @@ import { MeasurementApi } from '../classes';
 import log from '../../log';
 import user from '../../user';
 import getImageAttributes from '../lib/getImageAttributes';
-import guid from '../../utils/guid.js';
 import getLabel from '../lib/getLabel';
 
 export default function({ eventData, tool, toolGroupId, toolGroup }) {
@@ -73,31 +72,12 @@ export default function({ eventData, tool, toolGroupId, toolGroup }) {
       additionalProperties
     );
 
-    // Get the related measurement by the measurement number and use its location if defined
-    const relatedMeasurement = collection.find(
-      t =>
-        t.lesionNamingNumber === measurement.lesionNamingNumber &&
-        t.toolType === tool.parentTool &&
-        t.patientId === imageAttributes.patientId
-    );
-
-    // Use the related measurement location if found and defined
-    if (relatedMeasurement && relatedMeasurement.location) {
-      measurement.location = relatedMeasurement.location;
-    }
-
-    // Use the related measurement description if found and defined
-    if (relatedMeasurement && relatedMeasurement.description) {
-      measurement.description = relatedMeasurement.description;
-    }
-
-    measurement._id = guid();
     const addedMeasurement = measurementApi.addMeasurement(
       tool.parentTool,
       measurement
     );
 
-    measurementData._id = measurement._id;
+    measurementData._id = addedMeasurement._id;
     measurementData.lesionNamingNumber = addedMeasurement.lesionNamingNumber;
   }
 
