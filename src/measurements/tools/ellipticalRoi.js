d5d7e6d5fafd0a4b2f043088bbf5725e5c16a5e8
Evren Ozkan <evren217@gmail.com>
2019-05-13 17:09:54 -0400

    fix(tools): Bump cornerstone-tools to 3.9.0 (#20)
    * fix(tools): Bump cornerstone-tools to 3.9.0
    - Fix display function for elliptical and rectangle roi tools
    
    * feat(tools): Add tool definitions for freehand and circle roi

--- src/measurements/tools/ellipticalRoi.js
+++ src/measurements/tools/ellipticalRoi.js
@@ -1,12 +1,10 @@
 const displayFunction = data => {
   let meanValue = ''
-  if (data.meanStdDev && data.meanStdDev.mean) {
-    meanValue = data.meanStdDev.mean.toFixed(2) + ' HU'
+  const { cachedStats } = data
+  if (cachedStats && cachedStats.mean) {
+    meanValue = cachedStats.mean.toFixed(2) + ' HU'
   }
   return meanValue
-  // let meanValue = data.meanStdDev && data.meanStdDev.mean || 0;
-  // return numberWithCommas(meanValue).toFixed(2) + ' HU';
-  //return data.meanStdDev.mean.toFixed(2);
 }
 
 export const ellipticalRoi = {
