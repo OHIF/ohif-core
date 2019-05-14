53e5467897ed9eb99ab701c1c99d3c8b6b0bc3f6
ivan.aksamentov <ivan.aksamentov@gmail.com>
2019-05-14 17:48:44 +0200

    fix: avoid mutating state in reducers

--- src/redux/reducers/extensions.js
+++ src/redux/reducers/extensions.js
@@ -1,18 +1,17 @@
-const defaultState = {};
+const defaultState = {}
 
 const extensions = (state = defaultState, action) => {
   switch (action.type) {
     case 'SET_EXTENSION_DATA':
-      const extensionName = action.extension;
-      const currentData = state[extensionName] || {};
-      const updatedData = Object.assign({}, currentData, action.data);
-      const updatedState = state;
-      state[extensionName] = updatedData;
-
-      return Object.assign({}, state, updatedState);
+      const extensionName = action.extension
+      const currentData = state[extensionName] || {}
+      const updatedData = Object.assign({}, currentData, action.data)
+      return Object.assign({}, state, {
+        extensionName: updatedData,
+      })
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default extensions;
+export default extensions
--- src/redux/reducers/servers.js
+++ src/redux/reducers/servers.js
@@ -1,31 +1,34 @@
 const defaultState = {
-  servers: []
-};
+  servers: [],
+}
 
 const servers = (state = defaultState, action) => {
   switch (action.type) {
     case 'ADD_SERVER':
-      const { servers } = state;
+      const { servers } = state
       const alreadyExists = servers.find(
         server => server.id === action.server.id
-      );
+      )
 
       if (alreadyExists) {
-        return state;
+        return state
       }
 
-      servers.push(action.server);
+      const newServers =
+        servers && servers.length > 0
+          ? servers.concat(action.server)
+          : [action.server]
 
-      if (servers.length === 1) {
-        servers[0].active = true;
+      if (newServers.length === 1) {
+        newServers[0].active = true
       }
 
-      return Object.assign({}, state, { servers });
+      return Object.assign({}, state, { servers })
     case 'SET_SERVERS':
-      return Object.assign({}, state, { servers: action.servers });
+      return Object.assign({}, state, { servers: action.servers })
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default servers;
+export default servers
