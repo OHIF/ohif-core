0faf071093c937bdefcc6af6d465ac68888b1d22
dannyrb <danny.ri.brown@gmail.com>
2019-05-14 11:23:07 -0400

    Tests for our viewport reducers

--- src/redux/reducers/viewports.test.js
+++ src/redux/reducers/viewports.test.js
@@ -53,9 +53,6 @@ describe('viewports reducer', () => {
     expect(updatedState.layout).toEqual(setViewportLayoutAction.layout)
   })
 
-  // If there were previous keys, this would have
-  // "merge" behavior, not a clear & set
-  // May be worth another test?
   it('should handle SET_VIEWPORT', () => {
     const viewportToSet = 0
     const setViewportAction = {
