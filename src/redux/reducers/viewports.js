c727d36456ef71fa336d3e5c8c325f593478cbdf
dannyrb <danny.ri.brown@gmail.com>
2019-05-14 11:23:07 -0400

    Tests for our viewport reducers

--- src/redux/reducers/viewports.js
+++ src/redux/reducers/viewports.js
@@ -1,5 +1,12 @@
-import cloneDeep from 'lodash.clonedeep';
-import merge from 'lodash.merge';
+import cloneDeep from 'lodash.clonedeep'
+import merge from 'lodash.merge'
+
+import {
+  SET_VIEWPORT,
+  SET_VIEWPORT_ACTIVE,
+  SET_VIEWPORT_LAYOUT,
+  CLEAR_VIEWPORT,
+} from './../constants/ActionTypes.js'
 
 const defaultState = {
   activeViewportIndex: 0,
@@ -7,41 +14,49 @@ const defaultState = {
     viewports: [
       {
         height: '100%',
-        width: '100%'
-      }
-    ]
+        width: '100%',
+      },
+    ],
   },
-  viewportSpecificData: {}
-};
+  viewportSpecificData: {},
+}
 
+/**
+ * @param {Object} [state=defaultState]
+ * @param {Object} action
+ * @param {string} [action.type]
+ * @param {number} [action.viewportIndex]
+ * @param {Object} [action.layout]
+ * @param {Object} [action.viewportSpecificData]
+ */
 const viewports = (state = defaultState, action) => {
-  let viewportSpecificData;
+  let viewportSpecificData
   switch (action.type) {
-    case 'SET_VIEWPORT_ACTIVE':
+    case SET_VIEWPORT_ACTIVE:
       return Object.assign({}, state, {
-        activeViewportIndex: action.viewportIndex
-      });
-    case 'SET_LAYOUT':
-      return Object.assign({}, state, { layout: action.layout });
-    case 'SET_VIEWPORT_SPECIFIC_DATA':
+        activeViewportIndex: action.viewportIndex,
+      })
+    case SET_VIEWPORT_LAYOUT:
+      return Object.assign({}, state, { layout: action.layout })
+    case SET_VIEWPORT:
       const currentData =
-        cloneDeep(state.viewportSpecificData[action.viewportIndex]) || {};
-      viewportSpecificData = cloneDeep(state.viewportSpecificData);
+        cloneDeep(state.viewportSpecificData[action.viewportIndex]) || {}
+      viewportSpecificData = cloneDeep(state.viewportSpecificData)
       viewportSpecificData[action.viewportIndex] = merge(
         {},
         currentData,
         action.data
-      );
+      )
 
-      return Object.assign({}, state, { viewportSpecificData });
-    case 'CLEAR_VIEWPORT_SPECIFIC_DATA':
-      viewportSpecificData = cloneDeep(state.viewportSpecificData);
-      viewportSpecificData[action.viewportIndex] = {};
+      return Object.assign({}, state, { viewportSpecificData })
+    case CLEAR_VIEWPORT:
+      viewportSpecificData = cloneDeep(state.viewportSpecificData)
+      viewportSpecificData[action.viewportIndex] = {}
 
-      return Object.assign({}, state, { viewportSpecificData });
+      return Object.assign({}, state, { viewportSpecificData })
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default viewports;
+export default viewports
