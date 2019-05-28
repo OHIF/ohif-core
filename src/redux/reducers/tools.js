a54a9d3d201f5d56bbacccfe25993620108af56f
dannyrb <danny.ri.brown@gmail.com>
2019-05-28 13:05:50 -0400

    Remove default config for tools

--- src/redux/reducers/tools.js
+++ src/redux/reducers/tools.js
@@ -1,109 +1,46 @@
-import log from '../../log';
+import log from '../../log'
 
 const defaultButtons = [
-  {
-    command: 'Pan',
-    type: 'tool',
-    text: 'Pan',
-    svgUrl: '/icons.svg#icon-tools-pan',
-    active: false
-  },
-  {
-    command: 'Zoom',
-    type: 'tool',
-    text: 'Zoom',
-    svgUrl: '/icons.svg#icon-tools-zoom',
-    active: false
-  },
-  {
-    command: 'Bidirectional',
-    type: 'tool',
-    text: 'Bidirectional',
-    svgUrl: '/icons.svg#icon-tools-measure-target',
-    active: false
-  },
-  {
-    command: 'StackScroll',
-    type: 'tool',
-    text: 'Stack Scroll',
-    svgUrl: '/icons.svg#icon-tools-stack-scroll',
-    active: false
-  },
-  {
-    command: 'reset',
-    type: 'command',
-    text: 'Reset',
-    svgUrl: '/icons.svg#icon-tools-reset',
-    active: false
-  },
-  {
-    command: 'Wwwc',
-    type: 'tool',
-    text: 'Manual',
-    svgUrl: '/icons.svg#icon-tools-levels',
-    active: true
-  },
-  {
-    command: 'setWLPresetSoftTissue',
-    type: 'command',
-    text: 'Soft Tissue',
-    svgUrl: '/icons.svg#icon-wl-soft-tissue',
-    active: false
-  },
-  {
-    command: 'setWLPresetLung',
-    type: 'command',
-    text: 'Lung',
-    svgUrl: '/icons.svg#icon-wl-lung',
-    active: false
-  },
-  {
-    command: 'setWLPresetLiver',
-    type: 'command',
-    text: 'Liver',
-    svgUrl: '/icons.svg#icon-wl-liver',
-    active: false
-  },
-  {
-    command: 'setWLPresetBrain',
-    type: 'command',
-    text: 'Brain',
-    svgUrl: '/icons.svg#icon-wl-brain',
-    active: false
-  }
-];
+  // {
+  //   command: 'Pan',
+  //   type: 'tool',
+  //   text: 'Pan',
+  //   svgUrl: '/icons.svg#icon-tools-pan',
+  //   active: false
+  // }
+]
 
 const tools = (state = { buttons: defaultButtons }, action) => {
   switch (action.type) {
     case 'SET_TOOL_ACTIVE':
-      const item = state.buttons.find(button => button.command === action.tool);
-      let buttons = [];
+      const item = state.buttons.find(button => button.command === action.tool)
+      let buttons = []
 
       if (item && item.type === 'tool') {
         buttons = state.buttons.map(button => {
           if (button.command === action.tool) {
-            button.active = true;
+            button.active = true
           } else if (button.type === 'tool') {
-            button.active = false;
+            button.active = false
           }
 
-          return button;
-        });
+          return button
+        })
       } else {
-        buttons = state.buttons;
-        log.warn(`Tool ${action.tool} not found`);
+        buttons = state.buttons
+        log.warn(`Tool ${action.tool} not found`)
       }
 
       return {
-        buttons
-      };
+        buttons,
+      }
     case 'SET_AVAILABLE_BUTTONS':
       return {
-        buttons: action.buttons
-      };
+        buttons: action.buttons,
+      }
     default:
-      return state;
+      return state
   }
-};
+}
 
-export default tools;
+export default tools
