a72e45bba288f5bd30c0107ff771c79b111ae890
dannyrb <danny.ri.brown@gmail.com>
2019-06-01 00:01:36 -0400

    Stub hotkeyscontext tests

---
+++ src/classes/HotkeysContext.test.js
@@ -0,0 +1,21 @@
+import HotkeysContext from './HotkeysContext.js';
+
+describe('HotKeysContext', () => {
+  let hotkeysContext;
+
+  beforeEach(() => {
+    hotkeysContext = new HotkeysContext();
+  });
+
+  // shape, and default values
+  // this.store = store;
+  // this.name = name;
+  // this.definitions = Object.assign({}, definitions);
+  // this.enabled = enabled;
+
+  // register();
+  // unregister();
+  // init();
+  // destroy();
+  // removed: extend();
+});
