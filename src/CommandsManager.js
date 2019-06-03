715381e96e4a89d5e421f6b1f859a75ccd70c6cd
dannyrb <danny.ri.brown@gmail.com>
2019-06-02 22:46:53 -0400

    Nuke old files

--- src/CommandsManager.js
+++
@@ -1,7 +0,0 @@
-import { CommandsManager } from './classes/CommandsManager.js';
-
-// Create commands namespace using a CommandsManager class instance
-const commands = new CommandsManager();
-
-// Export relevant objects
-export default commands;
--- src/HotkeysManager.js
+++
@@ -1,7 +0,0 @@
-import { HotkeysManager } from './classes/HotkeysManager.js';
-
-// Create commands namespace using a HotkeysManager class instance
-const hotkeys = new HotkeysManager();
-
-// Export relevant objects
-export default hotkeys;
