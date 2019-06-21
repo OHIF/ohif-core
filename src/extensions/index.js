f3c5610f48f03a9c317bfb4ed763759424a2f695
dannyrb <danny.ri.brown@gmail.com>
2019-06-20 21:51:51 -0400

    Remove unused code

--- src/extensions/index.js
+++ src/extensions/index.js
@@ -1,15 +1,9 @@
 import ExtensionManager from './ExtensionManager';
 import MODULE_TYPES from './MODULE_TYPES.js';
-// import { OHIFPlugin } from './OHIFPlugin';
-
-// Each plugin registers an entry point function to be called
-// when the loading is complete.
 
 export default {
-  // OHIFPlugin,
   ExtensionManager,
   MODULE_TYPES,
-  // entryPoints: {},
 };
 
 export { ExtensionManager, MODULE_TYPES };
