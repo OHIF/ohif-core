9ec7f11512fb2461f7dd44e018577221cf56209c
biharck <biharck@gmail.com>
2019-08-04 10:57:49 -0300

    feat(index.test.js): improviments on index test file

--- src/DICOMWeb/index.test.js
+++ src/DICOMWeb/index.test.js
@@ -1,7 +1,7 @@
 import * as DICOMWeb from './index.js';
 
 describe('Top level exports', () => {
-  test('have not changed', () => {
+  test('should export the modules getAttribute, getAuthorizationHeader, getModalities, getName, getNumber, getString', () => {
     const expectedExports = [
       'getAttribute',
       'getAuthorizationHeader',
@@ -14,5 +14,41 @@ describe('Top level exports', () => {
     const exports = Object.keys(DICOMWeb.default).sort();
 
     expect(exports).toEqual(expectedExports);
+  });
+
+  test('should be able to call getAttribute function', () => {
+    DICOMWeb.default.getAttribute = jest.fn();
+    DICOMWeb.default.getAttribute();
+    expect(DICOMWeb.default.getAttribute).toBeCalled();
+  });
+
+  test('should be able to call getAuthorizationHeader function', () => {
+    DICOMWeb.default.getAuthorizationHeader = jest.fn();
+    DICOMWeb.default.getAuthorizationHeader();
+    expect(DICOMWeb.default.getAuthorizationHeader).toBeCalled();
+  });
+
+  test('should be able to call getModalities function', () => {
+    DICOMWeb.default.getModalities = jest.fn();
+    DICOMWeb.default.getModalities();
+    expect(DICOMWeb.default.getModalities).toBeCalled();
+  });
+
+  test('should be able to call getName function', () => {
+    DICOMWeb.default.getName = jest.fn();
+    DICOMWeb.default.getName();
+    expect(DICOMWeb.default.getName).toBeCalled();
+  });
+
+  test('should be able to call getNumber function', () => {
+    DICOMWeb.default.getNumber = jest.fn();
+    DICOMWeb.default.getNumber();
+    expect(DICOMWeb.default.getNumber).toBeCalled();
+  });
+
+  test('should be able to call getString function', () => {
+    DICOMWeb.default.getString = jest.fn();
+    DICOMWeb.default.getString();
+    expect(DICOMWeb.default.getString).toBeCalled();
   });
 });
